5aa4100d8b499389a4f2754dff2bd67b3a11aeb5
Multazeem Mirza <154812525+GithubMM24@users.noreply.github.com>
2026-08-25 19:23:02 +0530

    Update CORS middleware to remove allowed origins
    Removed specific origins from CORS middleware configuration.

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,6 @@ base = declarative_base()
 app.add_middleware(
     
     CORSMiddleware,
-    allow_origins=('http://localhost:3000', "http://localhost:5173","https://crm-support-system-kjsreilzc-mirza24s-projects.vercel.app/"), 
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"] 
